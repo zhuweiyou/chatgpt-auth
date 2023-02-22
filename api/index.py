43cf75cb033f163d7000bc4194130266416d067e
zhuweiyou <i@zhuweiyou.com>
2023-02-22 21:55:11 +0800

    add

--- api/index.py
+++ api/index.py
@@ -6,7 +6,8 @@ class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'application/json; charset=utf-8')
+        # self.send_header('Content-type', 'application/json; charset=utf-8')
+        self.send_header('Content-type', 'text/plain')
         self.end_headers()
         auth = Authenticator(
             email_address='dhfkvu@outlook.com', password='aMAfTEDw')
